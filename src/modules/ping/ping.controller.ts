ca75343d979da35e0c507148052b2796a18a85bd
roasted-tum0r <anirbansamaddar07@gmail.com>
2025-10-21 23:30:50 +0530

    Dated: (21/10/2025 11:20pm)
    Added:
    
    Deployment happening

--- src/modules/ping/ping.controller.ts
+++ src/modules/ping/ping.controller.ts
@@ -5,30 +5,9 @@ import { UpdatePingDto } from './dto/update-ping.dto';
 
 @Controller('ping')
 export class PingController {
-  constructor(private readonly pingService: PingService) {}
-
-  @Post()
-  create(@Body() createPingDto: CreatePingDto) {
-    return this.pingService.create(createPingDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.pingService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.pingService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePingDto: UpdatePingDto) {
-    return this.pingService.update(+id, updatePingDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.pingService.remove(+id);
+  constructor(private readonly pingService: PingService) { }
+  @Get('/health')
+  healthCheck() {
+    return { status: 'OK', timestamp: new Date() };
   }
 }

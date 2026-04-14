3f2daa818bb3e659398b3f529393ebed796d8716
roasted-tum0r <anirbansamaddar07@gmail.com>
2026-04-15 00:16:45 +0530

    Changes made: Fixed an issue in upload module.

--- src/modules/image-upload/upload.module.ts
+++ src/modules/image-upload/upload.module.ts
@@ -1,11 +1,12 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import { UploadController } from "./upload.controller";
 import { CloudinaryProvider } from "src/config/cloudinary/cloudinary.provider";
 import { CloudinaryService } from "src/config/cloudinary/cloudinary.service";
-
+import { UploadRepo } from "./upload.repo";
+@Global()
 @Module({
   controllers: [UploadController],
-  providers: [CloudinaryProvider, CloudinaryService],
-  exports: [CloudinaryProvider, CloudinaryService],
+  providers: [CloudinaryProvider, CloudinaryService, UploadRepo],
+  exports: [CloudinaryProvider, CloudinaryService, UploadRepo],
 })
 export class UploadModule {}
